889c99c2280570cc922a8b740b26355034b590e5
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-08-31 23:55:11 -0600

    no external

--- deno/index.ts
+++
@@ -1,3 +0,0 @@
-export {ValidateColor,colorize,hexToRGB,joinColors,rgb, rgba} from 'https://agacdn.onrender.com/AgaDev:colors@1.0.0/functions.ts';
-export {FONTS,BACKGROUND,FOREGROUND} from 'https://agacdn.onrender.com/AgaDev:colors@1.0.0/constants.ts';
-export type {Color} from 'https://agacdn.onrender.com/AgaDev:colors@1.0.0/constants.ts';
